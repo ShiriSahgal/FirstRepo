b7d99647169425bcd47288548149c042c3b6cac4
Roshani <Roshani@DESKTOP-NM7PLAH>
2024-02-17 17:59:44 +0530

    commit

---
+++ IntroductionToJava/src/com/capgemini/introduction/HelloWorldDay1.java
@@ -0,0 +1,14 @@
+package com.capgemini.introduction;
+//comment is not executable 
+//First Program to demonstrate printing statement
+public class HelloWorldDay1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hey, Welcome to Day - 01 of Java Lecture "
+				+ "start with hello world program");
+		
+
+	}
+
+}
