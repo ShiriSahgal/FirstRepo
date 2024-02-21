27cd5c9fc621aba1fa3c8630b5ecb12865ce479c
Roshani <Roshani@DESKTOP-NM7PLAH>
2024-02-21 15:05:57 +0530

    commit

---
+++ JavaProgram/src/com/feb20/arthop.java
@@ -0,0 +1,23 @@
+package com.feb20;
+
+import java.util.Scanner;
+
+public class arthop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Number:");
+		int a = sc.nextInt();
+		//System.out.println("Enter Second Number:");
+		int b = sc.nextInt();
+				
+		System.out.println("Addition:"+(a+b)+"\nSubtraction:"+(a-b)+
+						"\nDivision:"+(a/b)+"\nMultiplication:"+(a*b)+"\nMode:"+(a%b));
+
+			}
+
+		}
+
+
+	
---
+++ JavaProgram/src/com/feb20/logicalop.java
@@ -0,0 +1,13 @@
+package com.feb20;
+
+public class logicalop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(4>=5 && 4==4);
+		System.out.println(4>=5 ||4==4);
+		System.out.println(!(7==5));
+
+	}
+
+}
